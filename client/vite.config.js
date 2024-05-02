f2cadc0a6eeedb85bcb6bb836c973f9903974f2c
Sameliah Quioh <sameliahquioh@Sameliahs-iMac.local>
2024-05-01 23:46:35 -0700

    adds vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,9 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
+// vite.config.js
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    framework: 'vitest',
-    globals: true,
-    environment: 'jsdom',
+export default {
+  build: {
+    rollupOptions: {
+      input: 'src/App.jsx'
+    }
   }
-})
+}
