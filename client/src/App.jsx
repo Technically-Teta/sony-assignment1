8262f06b8e2b2ef8015f0de0b1c51b8d5e695aff
Sameliah Quioh <sameliahquioh@Sameliahs-iMac.local>
2024-05-02 15:12:19 -0700

    adds Auth0 and Authentication Button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
+import Nav from './components/Nav/Nav';
+import Books from './components/Books/Books';
 import './App.css';
-import Events from './components/events';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Techtonica 2023 H2 events</h1>
-  <Events />
-  </div>
-
-  )
-  
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App"> 
+        <Router> {/* Wrap your Routes inside a Router */}
+          <Nav />
+          <Routes>
+            <Route path="/" exact component={Books} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
