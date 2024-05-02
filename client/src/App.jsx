d6b71e09a9e4520cd5bcc6cd0ea8a33da66934d3
Sameliah Quioh <sameliahquioh@Sameliahs-iMac.local>
2024-05-02 16:11:14 -0700

    adds auth0providerwithhistory to main.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
 import Nav from './components/Nav/Nav';
 import Books from './components/Books/Books';
+import Profile from "./components/Authentication/Profile";
+import Loading from "./components/Authentication/Loading";
+import AuthenticationButton from './components/Authentication/AuthenticationButton';
+
 import './App.css';
 
 class App extends React.Component {
   render() {
+
+    const { isLoading } = useAuth0();
+    const { user } = useAuth0();
+    if (isLoading) {
+      return <Loading />;
+    } 
+
+
+
+
+
     return (
       <div className="App"> 
         <Router> {/* Wrap your Routes inside a Router */}
           <Nav />
+          <AuthenticationButton />
           <Routes>
             <Route path="/" exact component={Books} />
+            <Route path="api/myprofile" element={<Profile user={user}/>} /> 
           </Routes>
         </Router>
       </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <Auth0ProviderWithHistory>
+  <App />
+  </Auth0ProviderWithHistory>
+   
   </React.StrictMode>,
 )
